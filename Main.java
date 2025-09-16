bb4056db293402c7a87319d2c0d994892bd82d77
KurNek0 <lyashukd6@gmail.com>
2025-09-16 18:47:05 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Ship titanic = new Ship(15, 20);
-        System.out.println(titanic.isWorthIt());
-
-        Ship blackPearl = new Ship(50, 5);
-        System.out.println(blackPearl.isWorthIt());
-    }
-}
